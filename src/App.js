838850525876e8a909f7fbc1c7bcb208f661fcd1
naveed-niazi <naveedabdullah700@gmail.com>
2020-07-20 01:36:11 +0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import './App.css';
@@ -8,9 +9,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
